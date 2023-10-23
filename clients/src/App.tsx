f55626e3126d4e34c5e5abaed144703bd6684c7c
yashrajoria <yashrajoria@gmail.com>
2023-10-24 02:01:16 +0530

    Server test

--- clients/src/App.tsx
+++ clients/src/App.tsx
@@ -129,7 +129,7 @@ function App() {
         <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
         <RefineSnackbarProvider>
           <Refine
-            dataProvider={dataProvider("http://localhost:8080/api/v1")}
+            dataProvider={dataProvider("https://property-dashboard-ysj2.onrender.com/api/v1")}
             notificationProvider={notificationProvider}
             ReadyPage={ReadyPage}
             catchAll={<ErrorComponent />}
